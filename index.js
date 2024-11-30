e814651c2f0bdc1207449279135f8f27d9f0040a
Chathra Navoda Ranasinghe <chathra96@gmail.com>
2024-11-30 09:52:24 +0530

    defined routes and added dependencies

--- index.js
+++ index.js
@@ -1,38 +1,98 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+
+// const app = express();
+// const port = 3000;
+
+// app.use(bodyParser.json());
+
+
+// mongoose.connect('mongodb+srv://navoda:HBbf6pnKe6038iYH@mycluster.cups3.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster');
+// const db = mongoose.connection;
+// db.on('error', (error) => console.error(error));
+// db.once('open', () => console.log('Connected to Database'));
+
+// app.post('/', (req, res)=>{
+//   const {name, age, email} = req.body
+//   const newUser = new User({ name: name, age: age, email: email });
+//     newUser.save();
+//     res.json('API is working!');
+// });
+
+// app.delete('/:id', async (req, res) => {
+//   const id = req.params.id;
+//  await User.findByIdAndDelete(id);
+//   res.json('Delete successfully');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on :${port}`);
+// });
+
+
+
+// const { Schema, model } = mongoose;
+// const userSchema = new Schema({
+//   name: String,
+//   age: Number,
+//   email: String
+// });
+// const User = model('User', userSchema);
+
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const mongoose = require('mongoose');
+const asyncHandler = require('express-async-handler');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
-const port = 3000;
-
+//?Middleware
+app.use(cors({ origin: '*' }))
 app.use(bodyParser.json());
+//? setting static folder path
+app.use('/image/products', express.static('public/products'));
+app.use('/image/category', express.static('public/category'));
+app.use('/image/poster', express.static('public/posters'));
 
-
-mongoose.connect('mongodb+srv://navoda:HBbf6pnKe6038iYH@mycluster.cups3.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster');
+const URL = process.env.MONGO_URL;
+mongoose.connect(URL);
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
+// Routes
+app.use('/categories', require('./routes/category'));
+app.use('/subCategories', require('./routes/subCategory'));
+app.use('/brands', require('./routes/brand'));
+app.use('/variantTypes', require('./routes/variantType'));
+app.use('/variants', require('./routes/variant'));
+app.use('/products', require('./routes/product'));
+app.use('/couponCodes', require('./routes/couponCode'));
+app.use('/posters', require('./routes/poster'));
+app.use('/users', require('./routes/user'));
+app.use('/orders', require('./routes/order'));
+app.use('/payment', require('./routes/payment'));
+app.use('/notification', require('./routes/notification'));
 
-app.delete('/:id', async (req, res) => {
-  const id = req.params.id;
- await User.findByIdAndDelete(id);
-  res.json('Delete successfully');
+
+// Example route using asyncHandler directly in app.js
+app.get('/', asyncHandler(async (req, res) => {
+    res.json({ success: true, message: 'API working successfully', data: null });
+}));
+
+// Global error handler
+app.use((error, req, res, next) => {
+    res.status(500).json({ success: false, message: error.message, data: null });
 });
 
-//TODO:  this
 
-
-app.listen(port, () => {
-  console.log(`Server is running on :${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 });
 
 
-
-const { Schema, model } = mongoose;
-const userSchema = new Schema({
-  name: String,
-  age: Number,
-  email: String
-});
-const User = model('User', userSchema);
